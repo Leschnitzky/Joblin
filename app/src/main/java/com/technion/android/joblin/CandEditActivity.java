a73a17dd9ecf236d42cbd717beba8c4d8e678e59
Leschnitzky <leschinskysergey@gmail.com>
2019-05-07 10:31:35 +0300

    Fixed issue with overlapping toolbars

--- app/src/main/java/com/technion/android/joblin/CandEditActivity.java
+++ app/src/main/java/com/technion/android/joblin/CandEditActivity.java
@@ -13,17 +13,9 @@ public class CandEditActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cand_edit);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar_cand);
+        toolbar.setTitle("");
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
--- app/src/main/java/com/technion/android/joblin/MainActivity.java
+++ app/src/main/java/com/technion/android/joblin/MainActivity.java
@@ -1,5 +1,6 @@
 package com.technion.android.joblin;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cand_edit);
+
+        Intent intent = new Intent(this,CandEditActivity.class);
+        startActivity(intent);
     }
 }
