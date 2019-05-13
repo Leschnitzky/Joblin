edffcdf1a4583500506a74945b0bef1df62193da
Leschnitzky <leschinskysergey@gmail.com>
2019-05-13 15:46:09 +0300

    Inserted some changes to layout

--- app/src/main/java/com/technion/android/joblin/CandProfPrefActivity.java
+++ app/src/main/java/com/technion/android/joblin/CandProfPrefActivity.java
@@ -2,12 +2,16 @@ package com.technion.android.joblin;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class CandProfPrefActivity extends AppCompatActivity {
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cand_prof_pref);
+
+
     }
 }
