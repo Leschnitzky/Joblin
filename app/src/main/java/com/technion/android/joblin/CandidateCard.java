c3a9c04aa8e618dca6bfc5b67b652bb42589b6e0
bar teller <bar3200@gmail.com>
2019-05-14 18:47:21 +0300

    swiping updates the database ~yay~

--- app/src/main/java/com/technion/android/joblin/CandidateCard.java
+++ app/src/main/java/com/technion/android/joblin/CandidateCard.java
@@ -1,6 +1,7 @@
 package com.technion.android.joblin;
 
 import android.content.Context;
+import android.support.annotation.NonNull;
 import android.support.v7.widget.GridLayout;
 import android.util.Log;
 import android.widget.ImageView;
@@ -8,6 +9,16 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.Transaction;
 import com.mindorks.placeholderview.SwipePlaceHolderView;
 import com.mindorks.placeholderview.annotations.Layout;
 import com.mindorks.placeholderview.annotations.Resolve;
@@ -18,11 +29,16 @@ import com.mindorks.placeholderview.annotations.swipe.SwipeInState;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOut;
 import com.mindorks.placeholderview.annotations.swipe.SwipeOutState;
 import com.sothree.slidinguppanel.SlidingUpPanelLayout;
+import com.technion.android.joblin.DatabaseAPI.Side;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
-@Layout(R.layout.cancard_view)
-public class CandidateCard {
+import static com.technion.android.joblin.DatabaseUtils.*;
+
+@Layout(R.layout.reccard_view)
+public class RecruiterCard {
     @View(R.id.profileImageView)
     ImageView profileImageView;
 
@@ -68,14 +84,21 @@ public class CandidateCard {
     @View(R.id.slidingpanel)
     SlidingUpPanelLayout slidingPanel;
 
-    private Candidate mProfile;
+    private Recruiter mProfile;
     private Context mContext;
     private SwipePlaceHolderView mSwipeView;
+    private final String swiper;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+    CollectionReference candidatesCollection = db.collection(CANDIDATES_COLLECTION_NAME);
+    CollectionReference recruitersCollection = db.collection(RECRUITERS_COLLECTION_NAME);
+    CollectionReference usersCollection = db.collection(USERS_COLLECTION_NAME);
+    CollectionReference jobCategoriesCollection = db.collection(JOB_CATEGORIES_COLLECTION_NAME);
 
-    public CandidateCard(Context context, Candidate profile, SwipePlaceHolderView swipeView) {
+    public RecruiterCard(Context context, Recruiter profile, SwipePlaceHolderView swipeView, final String swiper_Email) {
         mContext = context;
         mProfile = profile;
         mSwipeView = swipeView;
+        swiper = swiper_Email;
     }
 
     private String getSkillsString(List<String> skills, int maxLength)
@@ -88,18 +111,17 @@ public class CandidateCard {
         return skillsString.toString().substring(0,skillsString.toString().length()-2);
     }
 
-
     @Resolve
     public void onResolved(){
         Glide.with(mContext).load(mProfile.getImageUrl()).into(profileImageView);
         nameTxt.setText(String.format("%s %s", mProfile.getName(), mProfile.getLastName()));
-        positionScopeTxt.setText(mProfile.getScope());
-        EducationTxt.setText(mProfile.getEducation());
-        fullEducationTxt.setText(mProfile.getEducation());
-        SkillsTxt.setText(getSkillsString(mProfile.getSkillsList(),10));
-        fullSkillsTxt.setText(getSkillsString(mProfile.getSkillsList(),20));
+        positionScopeTxt.setText(mProfile.getRequiredScope());
+        EducationTxt.setText(mProfile.getRequiredEducation());
+        fullEducationTxt.setText(mProfile.getRequiredEducation());
+        SkillsTxt.setText(getSkillsString(mProfile.getRequiredSkillsList(),10));
+        fullSkillsTxt.setText(getSkillsString(mProfile.getRequiredSkillsList(),20));
         locationNameTxt.setText(mProfile.getJobLocation());
-        descTxt.setText(mProfile.getMoreInfo());
+        descTxt.setText(mProfile.getJobDescription());
 
         slidingPanel.addPanelSlideListener(new SlidingUpPanelLayout.PanelSlideListener() {
             @Override
@@ -120,20 +142,154 @@ public class CandidateCard {
         });
     }
 
-    @SwipeOut
-    public void onSwipedOut(){
-        Log.d("EVENT", "onSwipedOut");
-        mSwipeView.addView(this);
-    }
-
     @SwipeCancelState
     public void onSwipeCancelState(){
         Log.d("EVENT", "onSwipeCancelState");
     }
 
+    void candidateDoSwipe(final String candidateMail, final String recruiterMail, final Side side) {
+
+        DocumentReference docRef = candidatesCollection.document(candidateMail);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                        Candidate candidate = document.toObject(Candidate.class);
+                        if((side == Side.RIGHT) && (candidate.getNumberOfSwipesLeft() == 0)) {
+                            Log.d(TAG, "number of swipes is 0");
+                            Utils.noMoreSwipesPopUp(mSwipeView.getContext());
+                        } else {
+                            addSwipeDataForCandidate(candidateMail, recruiterMail, side);
+                        }
+                    } else {
+                        Log.d(TAG, "No such document");
+                        Utils.errorPopUp(mSwipeView.getContext(),"");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                    Utils.errorPopUp(mSwipeView.getContext(),"");
+                }
+            }
+        });
+    }
+
+    void addSwipeDataForCandidate(String candidateMail, String recruiterMail, Side side) {
+        addSwipeData(candidatesCollection, recruitersCollection, candidateMail, recruiterMail, side);
+    }
+
+    void addSwipeData(CollectionReference firstCollection,
+                      CollectionReference secondCollection,
+                      String firstMail,
+                      String secondMail,
+                      Side side) {
+
+        String sideString;
+        if(side == Side.RIGHT) {
+            sideString = "right";
+        } else {
+            sideString = "left";
+        }
+
+        final Map<String, Object> firstSwipesMapData = new HashMap<>();
+        firstSwipesMapData.put(EMAIL_KEY, secondMail);
+        firstSwipesMapData.put(SIDE_KEY, sideString);
+
+        final Map<String, Object> secondSwipesMapData = new HashMap<>();
+
+        final Map<String, Object> firstMatchesMapData = new HashMap<>();
+        firstMatchesMapData.put(EMAIL_KEY, firstMail);
+
+        final Map<String, Object> secondMatchesMapData = new HashMap<>();
+        secondMatchesMapData.put(EMAIL_KEY, secondMail);
+
+        if(side == Side.RIGHT) {
+            final DocumentReference mainDocRefOfFirst = firstCollection.document(firstMail);
+            final DocumentReference swipeDocRefOfFirst = firstCollection.document(firstMail).collection(SWIPES_COLLECTION_NAME).document(secondMail);
+            final DocumentReference swipeDocRefOfSecond = secondCollection.document(secondMail).collection(SWIPES_COLLECTION_NAME).document(firstMail);
+            final DocumentReference matchDocRefOfFirst = firstCollection.document(firstMail).collection(MATCHES_COLLECTION_NAME).document(secondMail);
+            final DocumentReference matchDocRefOfSecond = secondCollection.document(secondMail).collection(MATCHES_COLLECTION_NAME).document(firstMail);
+            db.runTransaction(new Transaction.Function<Boolean>() {
+                @Override
+                public Boolean apply(Transaction transaction) throws FirebaseFirestoreException {
+                    DocumentSnapshot snapshotMainFirst = transaction.get(mainDocRefOfFirst);
+                    DocumentSnapshot snapshotSwipeSecond = transaction.get(swipeDocRefOfSecond);
+                    DocumentSnapshot snapshotSwipeFirst = transaction.get(swipeDocRefOfFirst);
+                    boolean isMatch = false;
+                    if(snapshotSwipeSecond.exists()) {
+                        if(snapshotSwipeSecond.get(SIDE_KEY).equals("right")) {
+                            transaction.set(matchDocRefOfFirst, secondMatchesMapData);
+                            transaction.set(matchDocRefOfSecond, firstMatchesMapData);
+                            isMatch = true;
+                        }
+                        transaction.update(swipeDocRefOfSecond, secondSwipesMapData);
+                    } else {
+                        transaction.set(swipeDocRefOfSecond, secondSwipesMapData);
+                    }
+
+                    if(snapshotSwipeFirst.exists()) {
+                        transaction.update(swipeDocRefOfFirst, firstSwipesMapData);
+                    } else {
+                        transaction.set(swipeDocRefOfFirst, firstSwipesMapData);
+                    }
+
+                    Log.d(TAG, "DocumentSnapshot data: " + snapshotMainFirst.getData());
+                    if(snapshotMainFirst.exists()) {
+                        long numberOfSwipesLeft = snapshotMainFirst.getLong(NUMBER_OF_SWIPES_LEFT_KEY);
+                        transaction.update(mainDocRefOfFirst, NUMBER_OF_SWIPES_LEFT_KEY, numberOfSwipesLeft - 1);
+                    }
+
+                    return isMatch;
+                }
+            }).addOnSuccessListener(new OnSuccessListener<Boolean>() {
+                @Override
+                public void onSuccess(Boolean isMatch) {
+                    Log.d(TAG, "Transaction success!");
+                    if(isMatch) {
+                        Log.d(TAG, "It is a match!");
+                        Utils.matchPopUp(mSwipeView.getContext());
+                    }
+                }
+            })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Transaction failure.", e);
+                            Utils.errorPopUp(mSwipeView.getContext(),e.toString());
+                        }
+                    });
+        } else {
+            firstCollection.document(firstMail).collection(SWIPES_COLLECTION_NAME).document(secondMail)
+                    .set(firstSwipesMapData)
+                    .addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void aVoid) {
+                            Log.d(TAG, "DocumentSnapshot successfully updated!");
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Log.w(TAG, "Error updating document", e);
+                            Utils.errorPopUp(mSwipeView.getContext(),e.toString());
+                        }
+                    });
+        }
+
+    }
+
     @SwipeIn
     public void onSwipeIn(){
         Log.d("EVENT", "onSwipedIn");
+        candidateDoSwipe(swiper,mProfile.getEmail(),Side.RIGHT);
+    }
+
+    @SwipeOut
+    public void onSwipedOut(){
+        Log.d("EVENT", "onSwipedOut");
+        candidateDoSwipe(swiper,mProfile.getEmail(),Side.LEFT);
     }
 
     @SwipeInState
