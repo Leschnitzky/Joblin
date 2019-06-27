141b9487f0f4042d782f1f42795d59df962ea58e
bar teller <bar3200@gmail.com>
2019-06-27 09:53:17 +0300

    better logout icon

--- app/src/main/java/com/technion/android/joblin/FormCustomViewState.kt
+++ app/src/main/java/com/technion/android/joblin/FormCustomViewState.kt
@@ -10,7 +10,6 @@ class FormCustomViewState(holder: ViewHolder) : BaseFormViewState(holder) {
     private var value: String? = null
     init {
         val editText = holder.viewFinder.find(R.id.formElementValue) as ClearableEditText
-        val units = holder.viewFinder.find(R.id.formElementUnits) as Spinner
         value = editText.text.toString()
     }
 
